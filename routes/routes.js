45b0bce60e8e8a67e5210a18b0a25beff3dc9383
Sandeep Sharma <161045544+Sandeep0900@users.noreply.github.com>
2024-12-23 20:50:39 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,7 @@
 import express from 'express';
-import {homeController, ContactUserController} from "../controllers/homeController.js"
+import {homeController} from "../controllers/homeController.js"
 const routes = express.Router();
 
 routes.get("/", homeController);
-
-routes.post("/", ContactUserController)
-
 
 export default routes ;
